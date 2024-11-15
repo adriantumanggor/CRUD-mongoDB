ddf6a3c83c01d4b30a767ba186aca4a7743cdbf8
Muhammad Felda <ifel@ifel>
2024-11-15 08:18:41 +0700

    commit

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -5,3 +5,33 @@ export async function GET(req: NextRequest) {
     const users = await prisma.users.findMany();
     return NextResponse.json(users);
 }
+
+export async function POST(req: NextRequest) {
+    const searchParams = req.nextUrl.searchParams;
+    const name = searchParams.get('name');
+    const email = searchParams.get('email');
+    const phone = searchParams.get('phone');
+
+    if(!name || !email || !phone) {
+        return NextResponse.json(
+            { error: 'Semua field harus diisi' },
+            { status: 400 }
+        )
+    }
+
+    try {
+        const user = await prisma.users.create({
+            data: {
+                name: name,
+                email: email,
+                phone: phone
+            }
+        })
+        return NextResponse.json(user); 
+    } catch (error) {
+        return NextResponse.json(
+            { error: error },
+            { status: 500 }
+        )
+    }
+}
